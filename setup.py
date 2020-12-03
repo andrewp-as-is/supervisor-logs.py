2bda700f86dea0f66fc11ecd89fdd1d6af0726f4
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:26:58 +0300

    init

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='supervisor-logs',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
